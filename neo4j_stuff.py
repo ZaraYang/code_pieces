69a52296b4cbb38d97f3592926bb023da682426a
HeYang <49229238+ZaraYang@users.noreply.github.com>
2020-08-12 09:47:11 +0800

    Create neo4j_stuff.py

---
+++ neo4j_stuff.py
@@ -0,0 +1,16 @@
+删除所有节点和关系
+
+MATCH (n)
+OPTIONAL MATCH (n)-[r]-()
+DELETE n,r
+
+创建新的节点和关系或者返回已有的节点和关系
+并对新节点添加属性
+
+merge(n:boss{id:0})
+merge(s:emp{id:1,name:'bob'})
+with n,s,case
+when s.age is null then [1]
+else [0] end as array
+foreach (x in array|merge (s)-[r:relationship]->(n) set s.age = 10)
+return n,s
